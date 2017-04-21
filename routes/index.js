30b09c502f982bcc393f24f179d04dc31abef3c8
zhentaoo <1904974327@qq.com>
2017-04-21 17:07:54 +0800

    first commit

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+var child_process = require('child_process');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Auto Deploy Running' });
+});
+
+router.get('/js-tower', function(req, res, next) {
+  console.log('js-tower');
+  var option = {
+    'cwd': '/Users/zhentaoo/Code/js-tower',
+  };
+
+  res = child_process.spawnSync('git', ['pull'], option);
+  console.log('stdout here: \n' + res.stdout);
+
+  res = child_process.spawnSync('rm', ['-rf', './public'], option)
+  console.log('stdout here: \n' + res.stdout);
+
+  res = child_process.spawnSync('hexo', ['generate'], option)
+  console.log('stdout here: \n' + res.stdout);
+
+  console.log('update js-tower blog success');
+});
+
+module.exports = router;
