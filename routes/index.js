f33de6da01846bf954e3360186c3f75b18a4d7c4
zhentaoo <1904974327@qq.com>
2019-05-28 07:56:15 +0800

    add final-statement

--- routes/index.js
+++ routes/index.js
@@ -20,4 +20,17 @@ router.post('/js-tower', function(req, res, next) {
   res.send('<p>update success</p>');
 });
 
+router.post('/final-statement', function(req, res, next) {
+  console.log('final-statement');
+  var option = {
+    'cwd': '/data/final-statement',
+  };
+
+  result = child_process.spawnSync('git', ['pull'], option);
+  console.log('stdout here: \n' + result.stdout);
+
+  console.log('update final-statement blog success');
+  res.send('<p>update success</p>');
+});
+
 module.exports = router;
