04bb4b811de8e40ac8aa5de9bf8bea56b43933bd
lizhentao <liuke@xiaohongshu.com>
2020-10-29 17:41:59 +0800

    feat: add my-site trigger

--- routes/index.js
+++ routes/index.js
@@ -33,4 +33,18 @@ router.post('/final-statement-private', function(req, res, next) {
   res.send('<p>update success</p>');
 });
 
+router.post('/my-site', function(req, res, next) {
+  console.log('my-site');
+  var option = {
+    'cwd': '/data/my-site',
+  };
+
+  result = child_process.spawnSync('git', ['pull'], option);
+  console.log('stdout here: \n' + result.stdout);
+
+  result = child_process.spawnSync('npm', ['run build'], option);
+  console.log('update my-site blog success');
+  res.send('<p>update success</p>');
+});
+
 module.exports = router;
